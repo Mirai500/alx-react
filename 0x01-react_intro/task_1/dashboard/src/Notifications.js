891a85992e4d61b2a5731717db01b3821436ff6a
Mirai500 <asifatumahbub001@gmail.com>
2023-11-09 04:39:04 +0100

    0x01-react-into

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,14 @@
+import './Notifications.css';
+import React from 'react';
+
+function Notifications() {
+    return (
+        <>
+            <div className="Notifications">
+                <p>Here is the list of notifications</p>
+            </div>
+        </>   
+    );
+};
+
+export default Notifications;
