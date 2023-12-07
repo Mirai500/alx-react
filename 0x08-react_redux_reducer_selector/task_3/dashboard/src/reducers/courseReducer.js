ed96c28eb5521de0e61555b48391e713893e18f5
Mirai500 <asifatumahbub001@gmail.com>
2023-12-07 03:45:56 +0100

    0x08-react_redux_reducer_selector-tasks

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,40 @@
+import { SELECT_COURSE,
+    UNSELECT_COURSE, 
+    FETCH_COURSE_SUCCESS 
+   } from '../actions/courseActionTypes'
+ 
+ const initialState = [];
+ 
+ function courseReducer(state = initialState, action) {
+   switch(action.type) {
+     case FETCH_COURSE_SUCCESS:
+       return action.data.map((course) => ({
+         ...course,
+         isSelected: false,
+       }));
+     case SELECT_COURSE:
+       return state.map((course, index) => {
+         const current = {
+           ...course,
+         };
+         if (course.id == action.index) current.isSelected = true;
+ 
+         return current;
+       });
+       case UNSELECT_COURSE:
+         return state.map((course) => {
+           const current = {
+             ...course,
+           };
+           if (course.id == action.index) current.isSelected = false;
+   
+           return current;
+         });
+   
+       default:
+         break;
+     }
+     return state;
+   };
+   
+   export default courseReducer;
